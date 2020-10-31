0e6284aa39c4c3e91d70c9ac16573a26ba4b7bd7
PabloAV2020 <pablo.aranedav@alumnos.usm.cl>
2020-10-31 18:11:04 -0300

    "Footer style background colot and divs"

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,32 +1,70 @@
 <template>
     <footer>
-        <br>
-        <br>
+        <div class="top">
+                <div>
+                    <h1>¿QUIERES VISITARNOS?</h1>
+                    <p>Te damos nuestra información de contacto</p>
+                    <p style="font-size:14px; color:#dd9f0d;"> Whatsapp:</p><p> +56 9 974202473</p>
+                    <p style="font-size:14px; color:#dd9f0d;">Email: </p><p>maravillas.camping@gmail.com</p>
+                </div>
+                <div>
+                    <h1>  CONÓCENOS  </h1>
+
+                </div>
+                <div>
+                    <h1> SÍGUENOS </h1>
+                </div>
+        </div>
+        <div class="bottom">
         <p> © Todos los derechos reservados  - Las Maravillas Secret Lodge - 2020 </p>
         <a href="https://www.facebook.com/maravillas.secretlodge">Visitanos en Facebook</a>
         <br>
+        </div>
     </footer>  
 </template>
 
 
 <script>
-
+export default {
+	props: ['navLinks'],
+}
 </script>
 
 <style scoped>
-p{
+
+.top{
+    
+    display: flex;
+    justify-content: space-around;
+    text-align: left;   
+    }
+
+
+.top h1{
+    color:#c3c3c3;
+    font-size: 16px;
+}    
+
+.top p{
+    color:#c3c3c3;
+    font-size: 14px;
+} 
+
+
+
+.bottom p{
     color: white;
     font-size: 9pt;
     font-weight: bolder;
 }
 
 footer {
-    background-color: black;
+    background-color:#2c353c;
     padding: 10px;
     text-align: center;
 }
 
-footer a{
+.bottom a{
     color:aquamarine;
 
 
